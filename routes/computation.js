d59834ea4ec30f5868678ad44c30010e102c85d6
sagajayakumar <jayakumar1994@gmail.com>
2021-03-24 10:25:03 -0500

    added new js router and endpoint for compute

---
+++ routes/computation.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET my data page. */
+router.get('/', function(req, res, next) {
+    res.render('mydata', { title: 'Jaya Kumar Saga' });
+    const x = search_params.get("value");
+    res.send('abs applied to: ' + x + " is " + Math.abs(x));
+});
+
+// router.get('/', function(req, res, next) {
+//     res.render('mid');
+// })
+
+module.exports = router;
