ab4914c5a8ded9df80434de67a1c702721580cf0
sagajayakumar <jayakumar1994@gmail.com>
2021-03-24 11:56:00 -0500

    updated reading params from url

--- routes/computation.js
+++ routes/computation.js
@@ -3,12 +3,9 @@ var router = express.Router();
 
 /* GET my data page. */
 router.get('/', function(req, res, next) {
-    const x = search_params.get("value");
+    const x = request.params.value;
     res.send('abs applied to: ' + x + " is " + Math.abs(x));
 });
 
-// router.get('/', function(req, res, next) {
-//     res.render('mid');
-// })
 
 module.exports = router;
